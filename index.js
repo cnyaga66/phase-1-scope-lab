29027a76a4f21faebb070c547881065e61f4089d
cnyaga66 <cnyga66@gmail.com>
2024-03-24 16:57:14 +0300

    My scopelab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution in this file!
+var customerName =  "bob";
+
+function changeToUpperCase(customerName) {
+  return customerName.toUpperCase();
+}
+
+ 
+const result = changeToUpperCase("Bob");
+
+console.log(result);
+
+function bestCustomer(){
+    return bestCustomer();
+}
+var result = bestCustomer;
+
+console.log(result);
+
+var customerName =  "bob";
+function overwriteBestCustomer(){
+    return overwriteBestCustomer();
+}
+var result = overwriteBestCustomer("bob");
+
+console.log(result);
+
+const CustomerName = "Alice"
+
+function leastFavouriteCustomerName(CustomerName){
+    return leastFavouriteCustomerName();
+}
+ 
+const result = leastFavouriteCustomerName("Alice") ;
+  
+  console.log(result)
+
+
+
+
